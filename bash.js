1eddbb829d223d97c27030f4e4df3bd36599566e
Andy Gao <andygao1990@gmail.com>
2021-07-20 21:40:56 -0400

    commit

--- bash.js
+++ bash.js
@@ -1,15 +1,15 @@
+const pwd = require ('./pwd')
+
 process.stdout.write('prompt > ')
+
 process.stdin.on('data', (data) => {
-    if (data = 'pwd') {
-        process.stdout.write(process.cwd())
-        process.stdout.write('\nprompt >')
-    } else {
+    if (data.toString().trim() === 'pwd') {
+        pwd()
+}
+    else {
         const cmd = data.toString().trim()
 
         process.stdout.write('You typed: ' + cmd)
         process.stdout.write('\nprompt >')
     }
-
-    
-    
 })
